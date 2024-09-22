925ac3810050f9b21d9ffc971e93042bd7a6597e
Terry <55205907+softerry@users.noreply.github.com>
2024-09-22 22:38:33 +0100

    created eye and eyeslash icon to show password as text

--- booking-app/src/login.js
+++ booking-app/src/login.js
@@ -1,40 +1,52 @@
+import React from 'react';
 import {FaLock} from 'react-icons/fa';
-import {FaUser} from 'react-icons/fa';
-import {FaEye} from 'react-icons/fa';
-import { FaEyeSlash } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
-import { FaGoogle } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
+import {FaUser, FaEye, FaEyeSlash, FaFacebook, FaGoogle, FaLinkedin} from 'react-icons/fa';
 import './index.css'
+import { useState } from 'react';
+
+
+
 function Login(){
-    // Login form
+    // set useState
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const PasswordToggle = () =>{
+        setIsPasswordVisible(!isPasswordVisible);
+    }
     return(
       <div className="container">
-        <form action="#">
-        <h1>Login</h1>
-        <span className='social-icons'><FaFacebook/><FaGoogle/><FaLinkedin/></span>
-        <div className="input-box">
-       <FaUser className='icon'/>
-        <input type="text" placeholder="Email" required/>
-        </div>
-        <div className='input-box'>
-       <FaLock className='icon'/>
-        <input type="password" placeholder="Password" required/>
-        <span className='icon-button'><FaEye/><FaEyeSlash/></span>
-        </div>
-        <div className='remember-forget'>
-        <label><input type='checkbox'/>Remember me</label>
-        <a href=''>Forget Password</a>
-        </div>
-        <div className='submit-button'>
-            <input type='submit' value='Submit'/>
-        </div>
-        <div className='signup-link'>
-            <p>Don't have an account? <a href='#'>Register</a></p>
-        </div>
-        </form>
-      </div>
+        {/* login form */}
+            <form action="#">
+            <h1>Login</h1>
+                <div className='social-icons'>
+                    <div className='social-icon'><FaFacebook/></div>
+                    <div className='social-icon'><FaGoogle/></div>
+                    <div className='social-icon'><FaLinkedin/></div>
+                </div> 
+            <div className="input-box">
+                <FaUser className='icon'/>
+                <input type="text" placeholder="Email" required/>
+            </div>
+            <div className='input-box'>
+                <FaLock className='icon'/>
+                {/* Toggle between FaEye and FaEyeSlash */}
+                <span className='icon-button' onClick={PasswordToggle}>{isPasswordVisible ? < FaEyeSlash/>:<FaEye/>}</span>
+                  {/* Show or hide password based on state */}
+                <input placeholder="Password" type={isPasswordVisible ? 'text' : 'password'} required/>
+                
+            </div>
+            <div className='remember-forget'>
+                <label><input type='checkbox'/>Remember me</label>
+                <a href=''>Forget Password</a>
+            </div>
+            <div className='submit-button'>
+                <input type='submit' value='Submit'/>
+            </div>
+            <div className='signup-link'>
+                <p>Don't have an account? <a href='#'>Register</a></p>
+            </div>
+            </form>
+      </div>     
     )
+    
 }
-
 export default Login;
