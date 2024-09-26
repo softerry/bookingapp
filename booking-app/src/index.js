988a2a23bbff5186707c934048e0c007e4845068
Terry <55205907+softerry@users.noreply.github.com>
2024-09-26 10:13:58 +0100

    Signup form update

--- booking-app/src/index.js
+++ booking-app/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import SignupForm from './sigup';
 import Login from "./login";
+import Example from './example';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<SignupForm/>
+<Example/>
   </React.StrictMode>
 );
 
--- booking-app/src/sigup.js
+++ booking-app/src/sigup.js
@@ -1,8 +1,21 @@
 import { FaFacebook, FaGoogle, FaLinkedin } from "react-icons/fa";
 import './index.css';
+import { useState } from "react";
 
 
 function SignupForm (){
+    const [password, setPassword] = useState ('');
+    const [isvalid, setIsvalid] = useState (null);
+    
+    // creat Regex for password validation
+    const passwordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const checkPassword  = (e) => {
+        const value = e.target.value;
+        setPassword(value);
+        // check if password matches the regex
+        setIsvalid(passwordReg.test(value));
+    }
+
     return(
         <div className='sign-up-container'>
             <form action='#'>
@@ -19,7 +32,8 @@ function SignupForm (){
                 </div>
         
                 <div className="input-box-sign-up">
-                    <input type='password' placeholder='Enter Password' required/>
+                    <input type='password' value={password} onChange={checkPassword} placeholder='Enter Password' required/>
+                    {isvalid === null ? null : isvalid ?(<p> Password is valid!</p>): ( <p>Password must be at least 8 characters long, contain one uppercase letter, one lowercase letter, one number, and one special character.</p>)}
                 </div>
                 <div className="input-box-sign-up">
                     <input type='password' placeholder='Repeat Password' required/>
